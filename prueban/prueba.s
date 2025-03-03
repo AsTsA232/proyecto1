1db5a8b7ae9868b853584f1e38dc68f7f201e4f8
AsTsA232 <matehurgal@gmail.com>
2025-03-03 15:20:06 -0600

    ola

--- prueban/prueba.s
+++ prueban/prueba.s
@@ -1,3 +1,32 @@
 
 
 numero rn r0 
+sumuno rn r1
+sumdos rn r2
+suc rn r4
+
+	area pn, code, readonly
+	entry
+	export __main
+		
+__main
+	mov numero,#8
+	mov sumuno,#0
+	mov sumdos,#1
+	
+	
+ciclo	
+	
+	add suc,sumuno
+	add suc,sumdos
+	mov sumuno,sumdos
+	mov sumdos,suc
+	cmp numero,suc
+	mov suc,#0
+	beq final
+	b ciclo
+	
+final
+fin b fin
+	
+	end
