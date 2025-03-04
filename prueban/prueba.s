d98d5829b134027cb8e8c0fdb0e0749eb6c97a0d
AsTsA232 <matehurgal@gmail.com>
2025-03-03 21:08:32 -0600

    d

--- prueban/prueba.s
+++ prueban/prueba.s
@@ -4,6 +4,7 @@ numero rn r0
 sumuno rn r1
 sumdos rn r2
 suc rn r4
+punt rn r5
 
 	area pn, code, readonly
 	entry
@@ -16,15 +17,20 @@ __main
 	
 	mov sumuno,#0
 	mov sumdos,#1
-	
+	ldr punt,=0x20000000
 	
 ciclo	
-	
+;sucesion
 	add suc,sumuno
 	add suc,sumdos
 	mov sumuno,sumdos
 	mov sumdos,suc
 	cmp numero,suc
+;carga en la memoria
+	
+	str suc, [punt]
+	add punt,#1
+;reinicio del ciclo
 	mov suc,#0
 	beq final
 	b ciclo
